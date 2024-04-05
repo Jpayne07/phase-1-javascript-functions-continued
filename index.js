32c32083e762dccc3152a66fe8b564bdd025643e
Jacob <jacobpayne007@gmail.com>
2024-04-04 23:40:23 -0500

    Getting harder!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(elipses = "roller-skate"){ 
+    return `This Saturday, I want to ${elipses}!`
+}
+
+function mondayWork(ellipses2 = "go to the office") {
+    return `This Monday, I will ${ellipses2}.`
+}
+
+function saturdayFun (normal = 'roller-skate'){
+    return `This Saturday, I want to ${normal}!`
+}
+
+function wrapAdjective (flair = "*") {
+    return function(name = "special"){
+        return `You are ${flair}${name}${flair}!`
+    }
+}
